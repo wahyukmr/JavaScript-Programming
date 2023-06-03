d4041e773c9aad23e7948695fa6344ad3a4d4e79
wahyukmr <maryu9229@gmail.com>
2023-06-03 10:44:40 +0700

    [fix] ✅ complete asynchronous tasks

--- 16-Asynchronous/assignments.js
+++ 16-Asynchronous/assignments.js
@@ -43,47 +43,70 @@
 // }
 
 // * Exercises ES8 - ASYNC AWAIT:
-// {
-//   // TODO 1: Convert the below promise into async await
-//   // fetch("https://jsonplaceholder.typicode.com/users/")
-//   //   .then((response) => response.json())
-//   //   .then((data) => console.log(data));
+{
+  /**
+   * TODO 1: Convert the below promise into async await!
+   *  fetch("https://jsonplaceholder.typicode.com/users/")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+   */
+  (async function () {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/");
+    const data = await response.json();
+    console.log(data);
+  })();
 
-//   (async function () {
-//     try {
-//       const response = await fetch(
-//         "https://jsonplaceholder.typicode.com/users/"
-//       );
-//       const data = await response.json();
-//       console.log(data);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   })();
+  /**
+   * TODO 2 (ADVANCED):
+   * Update the function below from the video to also have async await for this line: fetch(url).then(resp => resp.json()). So there shouldn't be any .then() calls anymore! Don't get discouraged... this is a really tough one...
+   */
+  const urls2 = [
+    "https://jsonplaceholder.typicode.com/users",
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/albums",
+  ];
+  async function getData() {
+    const [users, posts, albums] = await Promise.all(
+      urls2.map((url) => fetch(url).then((resp) => resp.json()))
+    );
+    console.log("users", users);
+    console.log("posta", posts);
+    console.log("albums", albums);
+  }
+  // ANSWER:
+  (async function getData() {
+    const [users, posts, albums] = await Promise.all(
+      urls2.map(async (url) => {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+      })
+    );
+    console.log("users", users);
+    console.log("posta", posts);
+    console.log("albums", albums);
+  })();
 
-//   // TODO 2 (ADVANCED): Update the function below from the video to also have
-//   // async await for this line: fetch(url).then(resp => resp.json())
-//   // So there shouldn't be any .then() calls anymore!
-//   // Don't get discouraged... this is a really tough one...
-//   const urls2 = [
-//     "https://jsonplaceholder.typicode.com/users",
-//     "https://jsonplaceholder.typicode.com/posts",
-//     "https://jsonplaceholder.typicode.com/albums",
-//   ];
-
-//   (async function getData() {
-//     const [users, posts, albums] = await Promise.all(
-//       urls2.map((url) => fetch(url).then((resp) => resp.json()))
-//     );
-//     console.log("users", users);
-//     console.log("posta", posts);
-//     console.log("albums", albums);
-//   })();
-
-//   // TODO 3: Add a try catch block to the #2 solution in order to catch any errors. // Now, use the given array containing an invalid url, so you console.log  //your error with 'oooooops'.
-//   const urls = [
-//     "https://jsonplaceholder.typicode.com/users",
-//     "https://jsonplaceholdeTYPO.typicode.com/posts",
-//     "https://jsonplaceholder.typicode.com/albums",
-//   ];
-// }
+  // TODO 3: Add a try catch block to the #2 solution in order to catch any errors. Now, use the given array containing an invalid url, so you console.log your error with 'oooooops'.
+  const urls3 = [
+    "https://jsonplaceholder.typicode.com/users",
+    "https://jsonplaceholdeTYPO.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/albums",
+  ];
+  (async function getData() {
+    try {
+      const [users, posts, albums] = await Promise.all(
+        urls3.map(async (url) => {
+          const response = await fetch(url);
+          const data = await response.json();
+          return data;
+        })
+      );
+      console.log("users", users);
+      console.log("posta", posts);
+      console.log("albums", albums);
+    } catch (error) {
+      console.log(`oooooops ${error}`);
+    }
+  })();
+}
